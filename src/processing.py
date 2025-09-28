d0346f926ddbbfdc50100cb80a5496faa491a7d3
Алена Гаврикова <moonstoris1999@gmail.com>
2025-09-28 11:49:34 +0700

    Fixed errors in the processing.py module

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,24 @@
 from datetime import datetime
 from typing import Any
 
-def filter_by_state(transactions: list[dict[str, Any]], state: str = 'EXECUTED') -> list[dict[str, Any]]:
+
+def filter_by_state(
+        transactions: list[dict[str, Any]], state: str = 'EXECUTED'
+) -> list[dict[str, Any]]:
     """
-    Функция, которая фильтрует список транзакций по статусу (state).
+    Функция, которая фильтрует список транзакций по статусу
+    (state).
     """
-    return [transaction for transaction in transactions if transaction.get('state') == state]
+    return [transaction for transaction in transactions if
+            transaction.get('state') == state]
 
 
-
-def sort_by_date(transactions: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
+def sort_by_date(
+        transactions: list[dict[str, Any]], reverse: bool = True
+) -> list[dict[str, Any]]:
     """
-    Функция, которая сортирует список транзакций по дате (date) в убывающем порядке по умолчанию.
+    Функция, которая сортирует список транзакций по дате
+    (date) в убывающем порядке по умолчанию.
     """
     def date_key(transaction: dict[str, Any]) -> datetime:
         return datetime.fromisoformat(transaction['date'])
